43f2f3756d4dfad60b6f1abaf269639365811d58
tiendung1411 <dungdthe163862@fpt.edu.vn>
2024-12-11 22:40:09 +0700

    initialize backend project

---
+++ ChatAppAPI/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatAppAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppAPI.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.UserName} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            await Clients.Group(conn.ChatRoom)
+                .SendAsync("ReceiveMessage", "admin", $"{conn.UserName} has joined {conn.ChatRoom}");
+        }
+    }
+}
